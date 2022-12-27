79b1bc45e7b904657ac509fe2c9fb8eb4a701314
Ward Abdelwahhab <wahhab.ward@gmail.com>
2022-12-27 18:46:29 +0300

    added layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './lib/components/header/header.component';
+import { FooterComponent } from './lib/components/footer/footer.component';
+import {MaterialModule} from "./material/material.module";
+import { AppLayoutComponent } from './lib/components/app-layout/app-layout.component';
+import { LayoutComponent } from './lib/components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AppLayoutComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
