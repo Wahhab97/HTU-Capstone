4cbacc29e9587107b34fc234b0cf70369ce023a1
Ward Abdelwahhab <wahhab.ward@gmail.com>
2022-12-31 14:26:13 +0300

    about section done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {AboutComponent} from "./pages/about/about.component";
-import {HeaderComponent} from "./lib/components/header/header.component";
 import {NotFoundComponent} from "./pages/screens/not-found/not-found.component";
 
 const routes: Routes = [
