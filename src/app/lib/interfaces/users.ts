32592549bfea91f9de51d9ecbce6e4831841ca96
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-01-02 13:19:21 +0300

    created users interface

---
+++ src/app/lib/interfaces/users.ts
@@ -0,0 +1,10 @@
+export interface Users {
+  id: string,
+  firstName: string,
+  lastName: string,
+  phone: number,
+  role: "user" | "admin" | "super-admin",
+  email: string,
+  country: string,
+  dateOfBirth: string
+}
