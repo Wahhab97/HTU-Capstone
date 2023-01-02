a5a3596c4f763d2080f5818bac03ffa0189135ba
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-01-02 15:24:32 +0300

    addUser and getUsers functions made

--- src/app/lib/services/users.service.ts
+++ src/app/lib/services/users.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
+import {User} from "../interfaces/user";
+import {from, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
-  addUser() {
-
+  constructor(private firestore: AngularFirestore) {
+    this.userCollection = this.firestore.collection("Users");
+  }
+  userCollection!: AngularFirestoreCollection<User>;
+  addUser(user: User) {
+    from(this.userCollection.add(user))
+  }
+  getUsers(): Observable<User[]> {
+    return this.userCollection.valueChanges();
   }
 }
