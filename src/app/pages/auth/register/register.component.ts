fcab4a2f3dfdba17f6ecb0b56c5d749ecdb138c9
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-01-02 15:37:13 +0300

    basic structure of register form done

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  registerForm= new FormGroup({
+    firstNameControl: new FormControl("", [Validators.required]),
+    lastNameControl: new FormControl("", [Validators.required]),
+    emailControl: new FormControl("", [Validators.required, Validators.email]),
+    passwordControl: new FormControl("", [Validators.required]),
+    confirmPasswordControl: new FormControl("", [Validators.required]),
+    dateOfBirthControl: new FormControl("", [Validators.required])
+  });
 }
