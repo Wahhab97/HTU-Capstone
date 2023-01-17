8c6fbbd6c3444abd6a3fc9b5a6c7b0a2e62b7652
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-01-17 12:14:21 +0300

    created style for the home but not yet functional

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
-import data from '../screens/not-found/companies.json'
+import {Component, OnInit} from '@angular/core';
 import {FilestorageService} from "../../lib/services/storage/filestorage.service";
 import {StartupsService} from "../../lib/services/startups/startups.service";
 import {SectorsService} from "../../lib/services/sectors/sectors.service";
+import {Sector} from "../../lib/interfaces/sector";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   constructor(private storage: FilestorageService, private startupsService: StartupsService, private sectorsService: SectorsService) {
   }
-  //
 
+  sectorsArray: Sector[] = [];
 
+  ngOnInit() {
+    this.sectorsService.getSectors().subscribe((response) => {
+      if(response) {
+        this.sectorsArray = response;
+      }
+    });
+  }
 }
