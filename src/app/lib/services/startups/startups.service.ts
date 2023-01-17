0c53d0da57802ca08af2ef0c3ca433c5064941ec
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-01-17 12:13:04 +0300

    added getStartupByName and getStartupsBySector function

--- src/app/lib/services/startups/startups.service.ts
+++ src/app/lib/services/startups/startups.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
 import {Startup} from "../../interfaces/startup";
-import {from} from "rxjs";
+import {from, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,13 @@ export class StartupsService {
 
   addStartup(startup: Startup) {
     from(this.starupsCollection.add(startup));
+  }
+  getStartupsBySector(sectorName: String): Observable<Startup[]> {
+    return this.firestore
+      .collection<Startup>("Startups", ref => ref.where("sector", "array-contains", sectorName)).valueChanges();
+  };
+  getStartupByName(name: string): Observable<Startup[]> {
+    return this.firestore
+      .collection<Startup>('Startups', ref => ref.where("companyName", "==", name)).valueChanges();
   }
 }
