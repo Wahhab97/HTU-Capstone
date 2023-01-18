6ced7358b341c483a182fe6f4d0bbfc47d68eab7
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-01-18 13:06:04 +0300

    created noAuth Guard and added it to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,24 @@ import {HomeComponent} from "./pages/home/home.component";
 import {AboutComponent} from "./pages/about/about.component";
 import {NotFoundComponent} from "./pages/screens/not-found/not-found.component";
 import {AdminAuthGuard} from "./lib/guards/admin-auth.guard";
+import {NoAuthGuard} from "./lib/guards/no-auth.guard";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
-    pathMatch: "full"
+    pathMatch: "full",
+    canActivate: [NoAuthGuard]
   },
   {
     path: "about",
-    component: AboutComponent
+    component: AboutComponent,
+    canActivate: [NoAuthGuard]
   },
   {
     path: "auth",
-    loadChildren: () => import("./pages/auth/auth.module").then(m => m.AuthModule)
+    loadChildren: () => import("./pages/auth/auth.module").then(m => m.AuthModule),
+    canActivate: [NoAuthGuard]
   },
   {
     path: "admin",
@@ -29,6 +33,7 @@ const routes: Routes = [
     path: "sectors",
     loadChildren: () => import("./pages/sector-details/sector-details.module")
       .then(m => m.SectorDetailsModule),
+    canActivate: [NoAuthGuard]
   },
   {
     path: "startups",
