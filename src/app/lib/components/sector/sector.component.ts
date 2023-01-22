1f3d7c3bf0d6329eb0fac08dadbfd465a62cd914
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-01-22 15:05:20 +0300

    added pictures to home page

--- src/app/lib/components/sector/sector.component.ts
+++ src/app/lib/components/sector/sector.component.ts
@@ -1,15 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Sector} from "../../interfaces/sector";
 import {Router} from "@angular/router";
+import {StartupsService} from "../../services/startups/startups.service";
+import {Startup} from "../../interfaces/startup";
 
 @Component({
   selector: 'app-sector',
   templateUrl: './sector.component.html',
   styleUrls: ['./sector.component.css']
 })
-export class SectorComponent {
-  constructor(private router: Router) {}
+export class SectorComponent implements OnInit{
+  constructor(private router: Router, private startupsService: StartupsService) {}
   @Input() childSector: Sector = {sectorName:"", count:0};
+
+  startups: Startup[] = [];
+  ngOnInit() {
+    this.startupsService.getStartupsBySector(this.childSector.sectorName).subscribe({
+      next: (value:Startup[]) => {
+        if(value[0]) {
+          this.startups = value;
+        }
+      }
+    });
+    console.log(this.startups);
+  }
 
   goToSector(val:string) {
     this.router.navigate(["sectors/"+val]);
